9dbc6d1bb919ed7b460b9447ea40fa258b95a834
LukaErCaiman <38765071+LukaErCaiman@users.noreply.github.com>
2021-11-01 18:57:13 -0400

    Codigo de estructuras + generadores
    añadido y editado codigo a las estructuras, y codigo muy basico a los generadores para el inicio del juego.

--- Variables.c
+++ Variables.c
@@ -2,18 +2,36 @@
 #include <stdio.h>
 #include <sys/types.h>
 
-struct Ciudades
+/* Estructura que representa a las ciudades del juego 
+id - Numero de la ciudad
+Instalaciones - arreglo que guarda que elementos existen en la ciudad.
+    Se guardan en flags dependiendo del indice del arreglo:
+    0 - es 1 si hay un centro pokemon
+    1 - */
+struct Ciudad
 {
     char id;
-    char instalaciones[];
+    char instalaciones[6];
+    char guaridaEn;
 };
 
 struct Pokemon
 {
-    char tipo;
     int hp;
-    int exp;
+    char tipo;
+    char nivel;
 };
+
+int GenerarCiudad()
+{
+    struct Ciudad* nueva = malloc(6); 
+}
+
+int GenerarPokemon(char nuevoTipo, char nuevoNivel)
+{
+    struct Pokemon* pkmn = malloc(6);
+    
+}
 
 
 int main()
