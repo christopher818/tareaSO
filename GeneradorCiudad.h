bb53c4b0e56d1a72750553f12bac148a25e258c9
christopher818 <christo.8.16@gmail.com>
2021-11-02 16:47:52 -0400

    intentando el make file

--- GeneradorCiudad.h
+++ GeneradorCiudad.h
@@ -1,2 +1,6 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "Variables.h"
+
 int bucleDeGeneradorDeCiudades(char random, char indice, char* instalacion[], char* cuentaGuaridas);
 int GenerarCiudades(struct Ciudad* Lista, char cantidadCiudades);
