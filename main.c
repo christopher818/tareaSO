56985a6e2ef42c0da645c1033df256ec12346ef7
christopher818 <christo.8.16@gmail.com>
2021-11-02 15:53:29 -0400

    colcando .h

---
+++ main.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "GeneradorCiudad.h"
+#include "PokemonMenu.h"
+#include "Variables.h"
+#include "InputFlusher.h"
+
+
+int main(int argc, char const *argv[])
+{
+    /* code */
+    return 0;
+}
