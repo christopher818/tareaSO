990a47f791b90068026afeb57b80f128f6d9a79b
LukaErCaiman <38765071+LukaErCaiman@users.noreply.github.com>
2021-11-02 17:28:01 -0400

    time añadido

--- GeneradorCiudad.h
+++ GeneradorCiudad.h
@@ -1,6 +1,2 @@
-#include <stdlib.h>
-#include <stdio.h>
-#include "Variables.h"
-
 int bucleDeGeneradorDeCiudades(char random, char indice, char* instalacion[], char* cuentaGuaridas);
 int GenerarCiudades(struct Ciudad* Lista, char cantidadCiudades);
