b01cce0bc487a6c2d7fb3851eb9f3bb8eb6b4f7f
LukaErCaiman <38765071+LukaErCaiman@users.noreply.github.com>
2021-11-03 14:25:30 -0400

    Create Apagado.c
    Archivo que contiene una funcion para apagar el juego una vez el jugador escriba "apagar", se recomienda usarlo en vez de "scanf" cada vez que se le pida al jugador algo

---
+++ Apagado.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+/*Funcion que sirve para tomar comandos en el juego.
+Si el jugador escribe "apagar", deber hacer una llamada a "exit()" y cerrar el juego.
+Entrada - Direccion de memoria para tomar la entrada del jugador.
+    -Si es "apagar", la funcion envia un mensaje de apagado y usa la funcion 'exit' para salir del programa.
+    -Si no, la funcion retorna 0.
+*/
+char EntradaDeComando(char* Entrada)
+{
+    scanf("%s", Entrada);
+
+    if(strcmp(Entrada, "apagar") == 0)
+    {
+        printf("Apagando juego.\n");
+        exit(0);
+    }
+    else
+        return 0;
+    
+    return 1;
+}
